4e94b7ae78a68474c18e01d67d2c04bd44bd717b
success <nkonwanasuccess@gmail.com>
2019-12-02 09:17:58 +0200

    saving

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,43 +1,96 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AlertController, NavController } from '@ionic/angular';
+import { AlertController, NavController, LoadingController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // signup(name: any, gender: any, email: any, password: any, race: any) {
-  //   throw new Error("Method not implemented.");
-  // }
-  signup(name, gender, email, password,age){
-    firebase.auth().createUserWithEmailAndPassword(email,password).then((credential) => {
-      this.db.collection('users').doc(credential.user.uid).set({
-        username : name,
-        email : email,
-        UserID: firebase.auth().currentUser.uid,
-        // ethnicity : ethnicity ,
-        gender:gender ,
-        age:age
-      }).then(() => {
-        this.navCtrl.navigateRoot('/login');
-        this.afAuth.auth.currentUser.updateProfile({
-          displayName : name,
-        })
-      }).catch(err => {
-        alert(err.message);
-      });
-    }).catch(err => {
-      alert(err.message);
-    });
-   }
-  loginUser(value: any) {
-    throw new Error("Method not implemented.");
-  }
 
-  constructor(public alertCtrl:AlertController,  
-    private db: AngularFirestore,
-    public navCtrl: NavController, 
-    private afAuth:AngularFireAuth) { }
+  user: Observable<User>;
+  ​
+    selectedFile = null;
+    uploadPercent: any;
+    downloadU: any;
+    uniqkey: any;
+    today: any = new Date();
+    date = this.today.getDate() + "" + (this.today.getMonth() + 1) + "" + this.today.getFullYear();
+    time = this.today.getHours() + "" + this.today.getMinutes();
+    dateTime = this.date + "" + this.time;
+  ​
+    progress
+  
+    constructor(public alertCtrl:AlertController,  
+      private afs: AngularFirestore,
+      public loadingCtrl: LoadingController,
+      private db: AngularFirestore,
+      public navCtrl: NavController, 
+      private afAuth:AngularFireAuth) {
+  
+        afAuth.auth.onAuthStateChanged((user) => {
+          if (user) {
+            // this.navCtrl.navigateRoot("landing");
+          } else {
+            this.navCtrl.navigateRoot("");
+          }
+        })
+      
+    
+       }
+       
+    async signup(email, password) {
+      const loading = this.loadingCtrl.create({
+        message: 'Registering, Please wait...'
+      });
+      (await loading).present();
+      await this.afAuth.auth.createUserWithEmailAndPassword(email, password).then(async (success) => {
+  ​
+        console.log(success);
+        (await loading).dismiss();
+  ​
+      }).catch(async (err) => {
+  ​
+        (await loading).dismiss();
+        
+        this.alertCtrl.create({
+          subHeader: err.message,
+          buttons: ['Ok']
+        }).then(
+          alert => alert.present()
+        );
+  ​
+      })
+    }
+    async login(email: string, password: string) {
+      await this.afAuth.auth.signInWithEmailAndPassword(email, password).then((success) => {
+        console.log(success);
+        this.navCtrl.navigateRoot("home");
+      }).catch((err) => {
+        this.alertCtrl.create({
+          // message: 'You can not order more than six',
+          subHeader: err.message,
+          buttons: ['Ok']
+        }).then(
+          alert => alert.present()
+        );
+      })
+    }
+    async sendPasswordResetEmail(passwordResetEmail: string) {
+      return await this.afAuth.auth.sendPasswordResetEmail(passwordResetEmail);
+    }
+  ​
+    async logout() {
+      await this.afAuth.auth.signOut().then((success) => {
+        console.log(success);
+        console.log("success");
+        this.navCtrl.navigateRoot("login");
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  
+
 }
