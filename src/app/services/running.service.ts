d6a96f78527a987ec69633f2e91790ebd7bea761
Lesley Ncube <lesley.t.ncube@gmail.com>
2019-12-04 13:44:12 +0200

    right home

--- src/app/services/running.service.ts
+++ src/app/services/running.service.ts
@@ -5,151 +5,151 @@ import 'firebase/firestore';
   providedIn: 'root'
 })
 export class RunningService {
-  database = firebase.database();
-  dbfire=firebase.firestore();
+//   database = firebase.database();
+//   dbfire=firebase.firestore();
   
-  user
-  clubs=[]
-  clubsTemp=[]
-  //INI values
-  newName: string="";
-  newAddress: string="";
-  newOpeningHours: string="";
-  newClosingHours: string="";
+//   user
+//   clubs=[]
+//   clubsTemp=[]
+//   //INI values
+//   newName: string="";
+//   newAddress: string="";
+//   newOpeningHours: string="";
+//   newClosingHours: string="";
 
-  editName: string="";
-  editAddress: string="";
-  editOpeningHours: string="";
-  editClosingHours: string="";
-  ///
+//   editName: string="";
+//   editAddress: string="";
+//   editOpeningHours: string="";
+//   editClosingHours: string="";
+//   ///
 
-  constructor()
-  { 
+//   constructor()
+//   { 
 
-  }
+//   }
 
-  //add a club
-  addClub(newName,newAddress,newOpeningHours,newClosingHours)
-  {
-    this.dbfire.collection("clubs").add({
-      name: newName,
-      address: newAddress,
-      openingHours: newOpeningHours,
-      closingHours: newClosingHours,
+//   //add a club
+//   addClub(newName,newAddress,newOpeningHours,newClosingHours)
+//   {
+//     this.dbfire.collection("clubs").add({
+//       name: newName,
+//       address: newAddress,
+//       openingHours: newOpeningHours,
+//       closingHours: newClosingHours,
       
-    }).then((data)=>{
+//     }).then((data)=>{
     
     
      
-      console.log(data)
-    }).catch((error)=>{
+//       console.log(data)
+//     }).catch((error)=>{
 
-    })
-  }
+//     })
+//   }
 
-  ///update a club
-updateTodo(clubs,editName,editAddress,editOpeningHours,editClosingHours)
-{
+//   ///update a club
+// updateTodo(clubs,editName,editAddress,editOpeningHours,editClosingHours)
+// {
  
-//name
-  this.dbfire.collection("clubs").doc(clubs.clubKey).update('name',editName).then((data)=> {
+// //name
+//   this.dbfire.collection("clubs").doc(clubs.clubKey).update('name',editName).then((data)=> {
    
-    console.log("Document name successfully updated!",data);
-}).catch(function(error) {
-    console.error("Error updating document: ", error);
-});
-//address
-this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editAddress).then((data)=> {
+//     console.log("Document name successfully updated!",data);
+// }).catch(function(error) {
+//     console.error("Error updating document: ", error);
+// });
+// //address
+// this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editAddress).then((data)=> {
   
-  console.log("Document time successfully updated!",data);
-}).catch(function(error) {
-  console.error("Error updating document: ", error);
-});
+//   console.log("Document time successfully updated!",data);
+// }).catch(function(error) {
+//   console.error("Error updating document: ", error);
+// });
 
-//opening hours
-this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editOpeningHours).then((data)=> {
+// //opening hours
+// this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editOpeningHours).then((data)=> {
   
-  console.log("Document time successfully updated!",data);
-}).catch(function(error) {
-  console.error("Error updating document: ", error);
-});
-//closing hours
-this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editClosingHours).then((data)=> {
+//   console.log("Document time successfully updated!",data);
+// }).catch(function(error) {
+//   console.error("Error updating document: ", error);
+// });
+// //closing hours
+// this.dbfire.collection("clubs").doc(clubs.clubKey).update('address',editClosingHours).then((data)=> {
   
-  console.log("Document time successfully updated!",data);
-}).catch(function(error) {
-  console.error("Error updating document: ", error);
-});
-}
+//   console.log("Document time successfully updated!",data);
+// }).catch(function(error) {
+//   console.error("Error updating document: ", error);
+// });
+// }
 
-//retrieve a club
-///////get todos
-getTodos()
-{
- this.clubs=[]
- this.clubsTemp=[]
-  let ans=[]
-  let ans2=[]
-   user=this.readCurrentSession()
-  let userID=user.uid
+// //retrieve a club
+// ///////get todos
+// getTodos()
+// {
+//  this.clubs=[]
+//  this.clubsTemp=[]
+//   let ans=[]
+//   let ans2=[]
+//    user=this.readCurrentSession()
+//   let userID=user.uid
 
-  //
-return new Promise((resolve, reject) => {
-this.dbfire.collection("todos").get().then((querySnapshot) => {
-   querySnapshot.forEach((doc) => {
+//   //
+// return new Promise((resolve, reject) => {
+// this.dbfire.collection("todos").get().then((querySnapshot) => {
+//    querySnapshot.forEach((doc) => {
     
-    // ans.push(doc.data())
-     console.log(doc.id, '=>', doc.data());
-     this.clubsTemp.push({
-       todoKey: doc.id,
-       name: doc.data().name,
-       time: doc.data().time,
-       userID: doc.data().userID
-     })
-       console.log( this.clubsTemp,"club array")
-       console.log(name,"club array")
+//     // ans.push(doc.data())
+//      console.log(doc.id, '=>', doc.data());
+//      this.clubsTemp.push({
+//        todoKey: doc.id,
+//        name: doc.data().name,
+//        time: doc.data().time,
+//        userID: doc.data().userID
+//      })
+//        console.log( this.clubsTemp,"club array")
+//        console.log(name,"club array")
    
-       console.log( this.clubsTemp.length,"club array SIZE")
-   //  this.todoTemp.push()
+//        console.log( this.clubsTemp.length,"club array SIZE")
+//    //  this.todoTemp.push()
      
-   });
-   console.log( this.clubsTemp.length,"club array SIZE")
-   for(let x=0;x< this.clubsTemp.length;x++)
-   {
-    console.log( this.clubsTemp[x].userID,"userid at x")
+//    });
+//    console.log( this.clubsTemp.length,"club array SIZE")
+//    for(let x=0;x< this.clubsTemp.length;x++)
+//    {
+//     console.log( this.clubsTemp[x].userID,"userid at x")
 
-        if(this.clubsTemp[x].userID===userID)
-        {
-          this.clubs.push(this.clubsTemp[x])
+//         if(this.clubsTemp[x].userID===userID)
+//         {
+//           this.clubs.push(this.clubsTemp[x])
 
-        }
+//         }
 
-   }
-   resolve(this.clubs)
-});
+//    }
+//    resolve(this.clubs)
+// });
 
-});
-console.log(this.clubsTemp,"clubs array")
+// });
+// console.log(this.clubsTemp,"clubs array")
 
 
 
-console.log(ans,"ans array")
+// console.log(ans,"ans array")
  
 
-}
+// }
 
 
-///////delete todo
-deleteTodo(clubs)
-{
- this.dbfire.collection("todos").doc(clubs.todoKey).delete().then((data)=> {
-   console.log("Document successfully deleted!",data);
+// ///////delete todo
+// deleteTodo(clubs)
+// {
+//  this.dbfire.collection("todos").doc(clubs.todoKey).delete().then((data)=> {
+//    console.log("Document successfully deleted!",data);
    
-}).catch(function(error) {
-   console.error("Error removing document: ", error);
-});
+// }).catch(function(error) {
+//    console.error("Error removing document: ", error);
+// });
 
-}
+// }
 
 
 }
