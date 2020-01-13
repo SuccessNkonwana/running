ea686cc5b208417f2067510d2d461a286a605e00
Butinti-Juliet <julietp238@gmail.com>
2020-01-13 04:04:13 -0800

    saving

--- src/app/pages/add-event/add-event.page.ts
+++ src/app/pages/add-event/add-event.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { RunningService } from 'src/app/services/running.service';
 
 @Component({
   selector: 'app-add-event',
@@ -12,14 +11,9 @@ export class AddEventPage implements OnInit {
   user = {} as User;
   public eventForm: FormGroup;
 
-  club;
-  newName;
-  newAddress;
-  newOpeningHours;
-  newClosingHours;
 
   constructor(
-    private fb: FormBuilder,private eventService:RunningService
+    private fb: FormBuilder
   ) {
 
     
@@ -33,18 +27,6 @@ export class AddEventPage implements OnInit {
     },
     );
    }
-
-  
-   addEvent(club,newName,newAddress,newOpeningHours,newClosingHours)
-  {
-       this.newName = this.eventForm.get('event').value
-       this.newAddress = this.eventForm.get('Address').value
-       this.newOpeningHours = this.eventForm.get('Hours').value   
-       this.newClosingHours = this.eventForm.get('Close').value
-
-
-    this.eventService.addEvent(this.club,this.newName,this.newAddress,this.newOpeningHours,this.newClosingHours)
-  }
 
   ngOnInit() {
   }
