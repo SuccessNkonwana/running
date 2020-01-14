e457572bbc8c1b041fc8a34c118370f5baa13831
Butinti-Juliet <julietp238@gmail.com>
2020-01-14 05:21:28 -0800

    saving

--- src/app/pages/add-event/add-event.page.ts
+++ src/app/pages/add-event/add-event.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { RunningService } from 'src/app/services/running.service';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-add-event',
@@ -8,6 +10,15 @@ import { RunningService } from 'src/app/services/running.service';
   styleUrls: ['./add-event.page.scss'],
 })
 export class AddEventPage implements OnInit {
+
+  users: any;
+  defaultpic=true
+  theUser=[];
+  photoURL: string;
+  uploadPercent: number;
+  currentuser: string;
+  
+
   clubs=[]
   newName
   newAddress
@@ -19,7 +30,7 @@ export class AddEventPage implements OnInit {
 
 
   constructor(
-    private fb: FormBuilder,public runn: RunningService) {
+    private fb: FormBuilder,public runn: RunningService, private authService: AuthService,) {
 
     
     this.eventForm = fb.group({
@@ -34,6 +45,9 @@ export class AddEventPage implements OnInit {
     );
 
    this.clubs=[]
+
+   this.theUser=[]    
+    this.getdata()
    }
 
   ngOnInit() {
@@ -42,6 +56,53 @@ addEvent(newName,newAddress,newOpeningHours,newClosingHours,newPrice)
 {
 
       this.runn.addEvent(this.newName,this.newAddress,this.newOpeningHours,this.newClosingHours,this.newPrice)
+}
+
+uploadProfilePic(event){
+  this.authService.uploadProfilePic(event).subscribe((data:number)=>{
+    this.uploadPercent=data
+    console.log(this.uploadPercent)
+  })
+}
+pickImage(){
+  this.authService.pickImage();
+}
+getdata()
+{
+  return new Promise((resolve, reject) => {
+    this.runn.rtUsers().then(data =>{
+   
+      console.log( data.length);
+      for( let x = 0; x < data.length; x++ )
+      {
+       console.log(x);
+       
+      this.theUser.push({ 
+        userKey:  data[x].userKey,
+        // name:  data[x].name,
+        // age:  data[x].age,
+        // email:  data[x].email,
+        // gender:  data[x].gender,
+        photoURL:data[x].photoURL}
+        
+        )
+        // userKey: doc.id,
+        //    name: doc.data().displayName,
+        //    age: doc.data().Age,
+        //    Email: doc.data().Email,
+        //    gender: doc.data().gender,
+        //    photoURL: doc.data().photoURL
+  
+      }
+    console.log(this.theUser,"the LAST ONE vele" )
+         if(this.theUser[0].photoURL==null)
+         {
+            this.defaultpic=false;
+          
+         }
+   })
+  })
+
 }
 
 
